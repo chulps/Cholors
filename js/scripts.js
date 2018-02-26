edc97d31b5d5a5f0a3fc4cdb64d267e38ae50a69
Chulps <chuckoward@gmail.com>
2018-02-26 05:21:46 -0800

    cholors is finished

--- js/scripts.js
+++ js/scripts.js
@@ -103,3 +103,33 @@ $("#anc_stop").click(function(){
     }
 
 });
+
+$("#anc_copy").click(function(){
+	selectElementContents( document.getElementById('tbl1') );
+	function selectElementContents(el) {
+    var body = document.body, range, sel;
+    if (document.createRange && window.getSelection) {
+        range = document.createRange();
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        try {
+            range.selectNodeContents(el);
+            sel.addRange(range);
+        } catch (e) {
+            range.selectNode(el);
+            sel.addRange(range);
+        }
+    } else if (body.createTextRange) {
+        range = body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+		document.execCommand("Copy");
+}	
+	alert("Cholors copied!");
+
+});
+
+
+
+
