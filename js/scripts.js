ac42c27a6f620590836ae2cda40661b6db1d07df
Chulps <chuckoward@gmail.com>
2018-03-01 02:38:51 -0800

    fixed footer, fixed border

--- js/scripts.js
+++ js/scripts.js
@@ -7,37 +7,15 @@ function getRandomColor() {
 	return color;
 }
 
-//lets simplify. break down into simplest-case functions and then compose them.
-//lets make this guy do one job - he takes *one* td, and he sets a random color on it's background
-//and puts the color hex in the html
+
 function setRandomColor(td) {
-  //this is going to set all td's in document to the same color because jquery's $().css(...)
-  //applies the same provided properties to *all* elements the selctor returns, and $('td') will
-  //return all td's in the doc.
-  //lets just remove it
-  //$("td").css("background-color", getRandomColor());
+  
   var randomColor = getRandomColor();
   
-  //note here that we are passing td, not 'td' - td here is the *actual* td element, which was a param
-  //for setRandomColor(td)
+  
   $(td).css('background-color', randomColor);
   $(td).html(randomColor);
-  //we won't actually need any of this...
-  //but declaring the var colorX here, and then directly changing colorX somewhere else in another
-  //function (i.e. in hexc()) is a bad practice - it makes it unclear where the change is coming from.
-  //this is called 'implicit side effect`, best to avoid it generally
-  //var colorX = '';
-	//$('td').html(function() {
-  //  var x = $(this).css('backgroundColor');
-  //  hexc(x);
-  //  $("td").html(colorX);
-  //})
   
-  //just to show you, now that hexc() returns a value (see changes below), we'd do it like this:
-	//$('td').html(function() {
-  //  var x = $(this).css('backgroundColor');
-  //  $("td").html(hexc(x));
-  //}) 
 }
 
 var cnt = 1;
@@ -49,10 +27,7 @@ function hexc(colorval) {
     parts[i] = parseInt(parts[i]).toString(16);
     if (parts[i].length === 1) parts[i] = '0' + parts[i];
   }
-  //try to avoid writing functions that change a variable outside of the function. instead, return a value
-  //from the function, and assign/use that value where desired by calling this function from that place
   
-  //colorX = '#' + parts.join('');
 	return '[' + cnt + '] - ' + '#' + parts.join('');
 }
 
